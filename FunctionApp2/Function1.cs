cdeecd241caee926659baab380b8725f333739c0
Prathyusha Avula <prathyusha.avula@databeat.io>
2021-09-16 18:37:41 +0530

    Linked Function App1 to Azure Function

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -16,6 +16,7 @@ namespace FunctionApp2
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
+
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
