5c800db1c2497e351459f34b0e250963e732871a
Prathyusha Avula <prathyusha.avula@databeat.io>
2021-09-17 09:47:36 +0530

    Testing CICD approach

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -26,6 +26,7 @@ namespace FunctionApp1
             name = name ?? data?.name;
 
 
+
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "Function app 1- updated."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
